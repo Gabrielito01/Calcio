87afafa7560e38c7720b4cf12faa36464921e83e
Gabriel Barreto <gabrielbarretz@gmail.com>
2020-05-24 19:30:44 +0200

    Project Architecture

---
+++ Calcio.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calcio.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
